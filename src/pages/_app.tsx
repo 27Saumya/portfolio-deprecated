8acc12dea3946b5870cc70c83fab77729cf8ee68
27Saumya <patelsaumya767@gmail.com>
2023-10-08 17:44:54 +0530

    Add a very basic homepage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import Head from 'next/head';
+
+const theme = extendTheme({
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+  components: {
+    Link: {
+      baseStyle: {
+        "&:hover": { textDecoration: "none" },
+      },
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Head>
+        <title>Saumya</title>
+      </Head>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { ColorModeScript, extendTheme } from "@chakra-ui/react";
+import { Html, Head, Main, NextScript } from 'next/document';
+
+const theme = extendTheme({
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+});
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <ColorModeScript initialColorMode={theme.initialColorMode} />
         <Main />
         <NextScript />
       </body>
